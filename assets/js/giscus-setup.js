bca1ff25b6847408e7131dc917ebca0f542042cf
MJLee95 <107920103+MJLee95@users.noreply.github.com>
2026-05-20 05:36:22 +0000

    Deploying to gh-pages from @ MJLee95/mj95lee.github.io@610b2bc37404ad8b30c524c14397dc2bd653829e 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "MJLee95/MJLee95.github.io",
+    "data-repo": "MJLee95/mj95lee.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",

4d8529cb805191fd2270127246e57c6c99051e1a
MJLee95 <107920103+MJLee95@users.noreply.github.com>
2026-05-20 08:09:13 +0000

    Deploying to gh-pages from @ MJLee95/mjlee95.github.io@d91d7ddcf1bf3096455603bc14fba3dfffe5f568 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -419,11 +419,6 @@ ninja.data = [{
           description: "Monte Carlo-based scenario analysis for pharmaceutical production planning",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
           title: 'project 4',
           description: "another without an image",
